b6030c805049dec14c609d9063a2d94b1cd4ee4f
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2023-11-20 15:37:54 +0100

    testing añadido, los nueve test pasan las pruebas

--- src/js/app.js
+++ src/js/app.js
@@ -6,23 +6,25 @@ const itemTag = document.getElementsByTagName("h3");
 const imgCard = document.querySelector(".product-image");
 
 
-redColor.addEventListener("click", ()=>{
+redColor?.addEventListener("click", ()=>{
     imgCard.style.backgroundImage = 'url(./public/img/redcar.jpg)';
     imgCard.alt = 'Red car';
     cartButton.style.backgroundColor = "red";
     itemTag.style.backgroundColor = "red"
 });
 
-grayColor.addEventListener("click", ()=>{
+grayColor?.addEventListener("click", ()=>{
     imgCard.style.backgroundImage = 'url(./public/img/graycar.jpg)';
     imgCard.alt = 'Gray car';
     cartButton.style.backgroundColor = "darkgray";
     itemTag.style.backgroundColor = "darkgray";
 });
 
-blackColor.addEventListener("click", ()=>{
+blackColor?.addEventListener("click", ()=>{
     imgCard.style.backgroundImage = 'url(./public/img/blackcar.jpg)';
     imgCard.alt = 'Black car';
     cartButton.style.backgroundColor = "black";
     itemTag.style.backgroundColor = "black"
 });
+
+export { blackColor, redColor, grayColor }
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,103 +1,55 @@
 import {describe ,expect, it, beforeAll} from 'vitest'
-import {JSOM} from 'jsdom'
-import from "./js/app.js";
+import {JSDOM} from 'jsdom'
+import { redColor, grayColor, blackColor } from "../src/js/app";
 
-describe('APP',()=>{
-    let dom;
-    beforeAll(async () => {
-        dom = await JSDOM.fromFile("index.html", {
-          resources: "usable",
-          runScripts: "dangerously",
-        });
-      });
-      it("should render css", async () => {
-        let document = dom.window.document;
-        let link = document.querySelector("link");
-        console.log(link.href);
-        expect(link.href).toMatch(/\/src\/css\/style.css$/);
-      });
-})
-
-
-
-// COPIED FROM CHANGE-COLOUR KATA
-
-describe("Image Card", () => {
-  let dom;
-  
-  let imgCard;
-
-  let redColor;
-
-  let grayColor;
-
-  let blackColor;
-
-  let button;
-
-  beforeAll(async () => {
-    dom = await JSDOM.fromFile("./index.html", { runScripts: "dangerously" });
-
-    imgCard = document.querySelector(".product-image");
-    redColor = document.getElementById('redColor');
-    grayColor = document.getElementById('grayColor');
-    blackColor = document.getElementById('blackColor');
-    cartButton = document.getElementById('button');
-  });
-
-  it("should render the script in html", () => {
-    const document = dom.window.document;
-    const script = document.querySelector("script");
-    expect(script).not.toBeNull();
-  });
 
   //RED COLOR TEST
-  it("should exist the function redColor", async () => {
+  it("should exist the object redColor", async () => {
     expect(redColor).toBeDefined();
-    expect(typeof redColor).toBe("function");
+    expect(typeof redColor).toBe("object");
   });
 
   // in redColor function, the car image should be red
   test("redColor changes the car image to red", async () => {
-    redColor();
+    return redColor;
     expect(imgCard.style.backgroundImage).toBe('url(./public/img/redcar.jpg)');
   });
 
   it("should change button color to red", async () => {
-    redColor();
+    return redColor;
     expect(cartButton.style.backgroundColor).toBe("red");
   });
 
   //GRAY COLOR TEST
-  it("should exist the function grayColor", async () => {
+  it("should exist the object grayColor", async () => {
     expect(grayColor).toBeDefined();
-    expect(typeof grayColor).toBe("function");
+    expect(typeof grayColor).toBe("object");
   });
 
   // in grayColor function, the car image should be gray
   test("grayColor changes the car image to gray", async () => {
-    grayColor();
+    return grayColor;
     expect(imgCard.style.backgroundImage).toBe('url(./public/img/graycar.jpg)');
   });
 
   it("should change button color to gray", async () => {
-    grayColor();
+    return grayColor;
     expect(cartButton.style.backgroundColor).toBe("darkgray");
   });
 
   //BLACK COLOR TEST
-  it("should exist the function blackColor", async () => {
+  it("should exist the object blackColor", async () => {
     expect(blackColor).toBeDefined();
-    expect(typeof blackColor).toBe("function");
+    expect(typeof blackColor).toBe("object");
   });
 
   // in grayColor function, the car image should be gray
   test("blackColor changes the car image to black", async () => {
-    blackColor();
+    return blackColor;
     expect(imgCard.style.backgroundImage).toBe('url(./public/img/blackcar.jpg)');
   });
 
   it("should change button color to black", async () => {
-    blackColor();
+    return blackColor;
     expect(cartButton.style.backgroundColor).toBe("black");
   });
